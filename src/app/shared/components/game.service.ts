911788ce10ec287571f0aff7b2fe2623f48d7aed
Mario Winter <mariowinter.sg@gmail.com>
2024-06-13 22:13:27 +0200

    Card deck is filled when the game is started

--- src/app/shared/components/game.service.ts
+++ src/app/shared/components/game.service.ts
@@ -8,5 +8,25 @@ export class GameService {
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
-    constructor() {}
+    constructor() {
+        this.fillStackAtGameStart();
+        this.shuffleStack();
+    }
+
+    fillStackAtGameStart() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push("ace_" + i);
+            this.stack.push("clubs_" + i);
+            this.stack.push("diamonds_" + i);
+            this.stack.push("hearts_" + i);
+        }
+    }
+
+    shuffleStack() {
+        this.stack = this.shuffle(this.stack);
+    }
+
+    shuffle(array: string[]): string[] {
+        return array.sort(() => Math.random() - 0.5);
+    }
 }
