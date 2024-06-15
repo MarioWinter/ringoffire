933c0e908e29ff986aa4704023771cf71217bfc2
Mario Winter <mariowinter.sg@gmail.com>
2024-06-15 13:59:26 +0200

    material Design Button Add added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { GameService } from "../shared/components/game.service";
 import { PlayerComponent } from "../shared/components/player/player.component";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
     selector: "app-game",
     standalone: true,
-    imports: [CommonModule, PlayerComponent],
+    imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule],
     templateUrl: "./game.component.html",
     styleUrl: "./game.component.scss",
 })
