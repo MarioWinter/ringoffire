c24283bf296eb2846529e61e21aea4e5bf1928aa
Mario Winter <mariowinter.sg@gmail.com>
2024-06-15 13:18:50 +0200

    All player names are displayed

--- src/app/shared/components/player/player.component.ts
+++ src/app/shared/components/player/player.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-player',
-  standalone: true,
-  imports: [],
-  templateUrl: './player.component.html',
-  styleUrl: './player.component.scss'
+    selector: "app-player",
+    standalone: true,
+    imports: [],
+    templateUrl: "./player.component.html",
+    styleUrl: "./player.component.scss",
 })
 export class PlayerComponent {
-
+    @Input() playerName: string = "";
 }
